6d0bcfd5b17fc6abb8bc0d131bb97a22055925fc
zhangping168@gmail.com <zhangping168@gmail.com>
2016-11-29 16:12:26 -0500

    Update project name

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use(function (req, res, next) {
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-  console.log('Listening port '+ port + '...');
+  console.log('Listening port '+ PORT + '...');
 });
